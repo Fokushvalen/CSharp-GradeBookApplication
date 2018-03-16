f48b3f7bc2da67851dac3dd6ce6630aca2a238ba
Jonas Jørgensen <jonas.gj@hotmail.com>
2018-03-16 15:53:17 +0100

    Correcting "Add Support for Weighted Grading"

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -21,7 +21,7 @@ namespace GradeBook.GradeBooks
         {
             Name = name;
             Students = new List<Student>();
-
+            IsWeighted = IsWeighted;
         }
 
         public void AddStudent(Student student)
